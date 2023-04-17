2a93a034e223573ee28f516a1050afb7d0ca430c
ankitaAtGit <48355393+ankitaAtGit@users.noreply.github.com>
2023-04-17 11:45:44 +0530

    update

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -9,12 +9,14 @@ import * as styles from './Layout.module.css';
 import './Globals.css';
 
 const Layout = ({ props, children, disablePaddingBottom = false }) => {
+  useEffect(() => {
+    const scriptTag = document.createElement("script");
+    scriptTag.innerHTML = `var userloveq = []; function userlove(method, args) { (!!userloveMethods && userloveMethods[method]) ? userloveMethods[method](args) : userloveq.push({ method: method, args: args }); } (function (l, o, v, e) { if (l["a_id"]) { return; } l["a_id"] = e; l["dm"] = l.location.hostname; l[v] = l[v] || []; l[o] = {}; var s = document.createElement("script"); s.async = true; s.src = "https://cdn.userlove.io/wrapper.dev.js?v=" + Date.now(); var t = document.getElementsByTagName("script")[0]; t.parentNode.insertBefore(s, t); })(window, "userloveMethods", "userloveDL", "4ZHFVL8VB6")`;
+    document.head.appendChild(scriptTag);
+  }, []);
   return (
     <>
       <Helmet>
-       <script>
-            var userloveq = []; function userlove(method, args) { (!!userloveMethods && userloveMethods[method]) ? userloveMethods[method](args) : userloveq.push({ method: method, args: args }); } (function (l, o, v, e) { if (l["a_id"]) { return; } l["a_id"] = e; l["dm"] = l.location.hostname; l[v] = l[v] || []; l[o] = {}; var s = document.createElement("script"); s.async = true; s.src = "https://cdn.userlove.io/wrapper.dev.js?v=" + Date.now(); var t = document.getElementsByTagName("script")[0]; t.parentNode.insertBefore(s, t); })(window, "userloveMethods", "userloveDL", "4ZHFVL8VB6")
-        </script>
         <link
           rel="stylesheet"
           type="text/css"
