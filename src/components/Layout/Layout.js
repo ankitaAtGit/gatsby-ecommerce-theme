bbfff3281cf111688faec5aa94d1e14bc6825d9d
ankitaAtGit <48355393+ankitaAtGit@users.noreply.github.com>
2023-04-17 11:38:07 +0530

    script change

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
-
 import Header from '../Header';
 import Footer from '../Footer';
 import * as styles from './Layout.module.css';
@@ -13,7 +12,9 @@ const Layout = ({ props, children, disablePaddingBottom = false }) => {
   return (
     <>
       <Helmet>
-        {/* Add any sitewide scripts here */}
+       <script>
+            var userloveq = []; function userlove(method, args) { (!!userloveMethods && userloveMethods[method]) ? userloveMethods[method](args) : userloveq.push({ method: method, args: args }); } (function (l, o, v, e) { if (l["a_id"]) { return; } l["a_id"] = e; l["dm"] = l.location.hostname; l[v] = l[v] || []; l[o] = {}; var s = document.createElement("script"); s.async = true; s.src = "https://cdn.userlove.io/wrapper.dev.js?v=" + Date.now(); var t = document.getElementsByTagName("script")[0]; t.parentNode.insertBefore(s, t); })(window, "userloveMethods", "userloveDL", "4ZHFVL8VB6")
+        </script>
         <link
           rel="stylesheet"
           type="text/css"
@@ -25,6 +26,7 @@ const Layout = ({ props, children, disablePaddingBottom = false }) => {
           charset="UTF-8"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
         />
+        
       </Helmet>
 
       <Header />
