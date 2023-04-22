c059ee7adb6b0f78c549488ea93bbfaaf3819d6a
ankitaAtGit <48355393+ankitaAtGit@users.noreply.github.com>
2023-04-22 11:19:06 +0530

    url change

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -13,7 +13,7 @@ const Layout = ({ children, disablePaddingBottom = false }) => {
     <>
       <Helmet>
         {/* Add any sitewide scripts here */}
-        <script type="text/javascript" src={'./js/main.js'} />
+        <script type="text/javascript" src={'https://cdn.jsdelivr.net/gh/ankitaAtGit/main/main.js'} />
         <link
           rel="stylesheet"
           type="text/css"
