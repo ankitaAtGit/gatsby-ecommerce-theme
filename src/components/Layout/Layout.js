eb2630108ebce04098a82be381c961480cbdda77
ankitaAtGit <48355393+ankitaAtGit@users.noreply.github.com>
2023-05-02 17:21:27 +0530

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -13,7 +13,7 @@ const Layout = ({ children, disablePaddingBottom = false }) => {
     <>
       <Helmet>
         {/* Add any sitewide scripts here */}
-        <script type="text/javascript" src={'https://cdn.jsdelivr.net/gh/ankitaAtGit/main/main.js'} />
+        <script type="text/javascript" src={'https://cdn.jsdelivr.net/gh/ankitaAtGit/main/main1.js'} />
         <link
           rel="stylesheet"
           type="text/css"
